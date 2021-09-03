b3b18ab404a44e3764eafb947439e1102846afdc
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-03 10:44:38 +0100

    crate org.djna.Introducer

--- introduceMe/src/org/djna/Introducer.java
+++ introduceMe/src/org/djna/Introducer.java
@@ -2,7 +2,7 @@ package org.djna;
 
 public class Introducer {
     public static void main(String[] args) {
-        System.out.println("Hello,I'm Dave from Ruislip and I play Go");
+        System.out.println("Hello,I'm Dave from Ruislip and I play Go online");
         System.exit(0);
     }
 }
