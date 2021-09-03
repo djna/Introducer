245a70691ff8fdd9366e73312a05c15531dd148f
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-03 10:42:10 +0100

    crate org.djna.Introducer

---
+++ introduceMe/src/org/djna/Introducer.java
@@ -0,0 +1,8 @@
+package org.djna;
+
+public class Introducer {
+    public static void main(String[] args) {
+        System.out.println("Hello,I'm Dave from Ruislip and I play Go");
+        System.exit(0);
+    }
+}
