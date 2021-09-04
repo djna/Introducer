330d27606905030c97138aa1f1de0b8fe1794c38
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-04 12:08:57 +0100

    simplified match

--- introduceMe/src/org/djna/RegExp.java
+++ introduceMe/src/org/djna/RegExp.java
@@ -7,15 +7,12 @@ public class RegExp {
 
     public static void main(String[] args) {
         String target = "one,two,three";
-        String regex = "(.+),.+,(.+)";
+        String regex = "two";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(target);
 
         if( matcher.find() ) {
-            System.out.printf("We have a match at %d\n", matcher.start());
-            for ( int g = 1; g <= matcher.groupCount(); g++){
-                System.out.printf("group %d=%s\n", g, matcher.group(g));
-            }
+            System.out.printf("We have a match at offset %d\n", matcher.start());
         }
     }
 }
